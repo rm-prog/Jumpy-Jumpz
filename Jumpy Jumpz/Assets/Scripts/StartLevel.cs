c90392b308c134ea0b7487c2aa57f9e619ac99f7
rm-prog <rminollari@gmail.com>
2023-11-12 16:39:33 +0100

    Fixed bug, where linearSpeed variable would increase before the player started moving.

--- Jumpy Jumpz/Assets/Scripts/StartLevel.cs
+++ Jumpy Jumpz/Assets/Scripts/StartLevel.cs
@@ -3,18 +3,21 @@ using UnityEngine;
 public class StartLevel : MonoBehaviour
 {
 
-    public GameObject StartPanel;
-    public GameObject GamePanel;
+    public GameObject startPanel;
+    public GameObject gamePanel;
+    public GameObject player;
 
     void Start()
     {
         Time.timeScale = 0;        
+        player.GetComponent<PlayerMovement>().enabled = false;
     }
 
     public void LevelStart()
     {
         Time.timeScale = 1;
-        StartPanel.SetActive(false);
-        GamePanel.SetActive(true);
+        startPanel.SetActive(false);
+        gamePanel.SetActive(true);
+        player.GetComponent<PlayerMovement>().enabled = true;
     }
 }
