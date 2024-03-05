a3f503bc5d21781a1ade194520da484fc27e2e54
rm-prog <rminollari@gmail.com>
2024-03-05 18:45:51 +0100

    Made SideWalls in Endlessrunner slippery; Made Endlessrunner stop when player is hit.

--- Jumpy Jumpz/Assets/Scripts/GameManager.cs
+++ Jumpy Jumpz/Assets/Scripts/GameManager.cs
@@ -19,6 +19,7 @@ public class GameManager : MonoBehaviour
         {
             GameOverPanel.SetActive(true);
             GamePanel.SetActive(true);
+            Time.timeScale = 0f;
         }
     }
 
