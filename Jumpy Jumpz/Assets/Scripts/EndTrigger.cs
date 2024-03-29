f82859a8afd9200f051fc6557035207a8e860952
rm-prog <rminollari@gmail.com>
2024-03-29 15:11:11 +0100

    Lock/Unlock Level System

--- Jumpy Jumpz/Assets/Scripts/EndTrigger.cs
+++ Jumpy Jumpz/Assets/Scripts/EndTrigger.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndTrigger : MonoBehaviour
 {
@@ -11,7 +12,19 @@ public class EndTrigger : MonoBehaviour
         // If player reaches end of level
         if (other.gameObject.layer == 6)
         {
+            UnlockNewLevel();
             gameManager.CompleteLevel();
         }
     }
+
+    void UnlockNewLevel()
+    {
+        if (SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
+        {
+            PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex + 1);
+            PlayerPrefs.SetInt("UnlockedLevel", PlayerPrefs.GetInt("UnlockedLevel") + 1);
+            PlayerPrefs.Save();
+        }
+    }
+
 }
--- Jumpy Jumpz/Assets/Scripts/LevelSelector.cs
+++ Jumpy Jumpz/Assets/Scripts/LevelSelector.cs
@@ -1,12 +1,28 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class LevelSelector : MonoBehaviour
 {
 
-    public int level;
+    public Button[] buttons;
 
-    public void OpenScene()
+    private void Awake()
+    {
+        int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
+        PlayerPrefs.SetInt("ReachedIndex", 3);
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].interactable = false;
+            if (i < unlockedLevel)
+            {
+                buttons[i].interactable = true;
+            }
+        }
+    }
+
+    public void OpenScene(int level)
     {
         SceneManager.LoadScene("Level " + level);
     }
