6bb8e46997363e9710972e9723da5b29c509c32d
rm-prog <rminollari@gmail.com>
2024-05-02 00:45:44 +0200

    Edited Level 2. Changed shadow position and local scale when player approaches the ground

--- Jumpy Jumpz/Assets/Scripts/ShadowFollow.cs
+++ Jumpy Jumpz/Assets/Scripts/ShadowFollow.cs
@@ -5,7 +5,7 @@ public class ShadowFollow : MonoBehaviour
 
     public Transform player;
 
-    private readonly float positionY = 0.08f;
+    private readonly float positionY = 0.09f;
 
     private float offsetX = 5.0f;
 
@@ -14,13 +14,12 @@ public class ShadowFollow : MonoBehaviour
 
     void FixedUpdate()
     {
-
         offsetX = player.position.y;
 
-        transform.position = new Vector3(player.position.x + offsetX, positionY, player.position.z);
+        if (offsetX < 0 ) transform.position = new Vector3(player.position.x, positionY, player.position.z);
+        else transform.position = new Vector3(player.position.x + offsetX, positionY, player.position.z);
 
-        transform.localScale = new Vector3(scaleMultiplier * player.position.y, transform.localScale.y, scaleMultiplier * player.position.y);
-
+        if (player.position.y > 0) transform.localScale = new Vector3(scaleMultiplier * player.position.y, transform.localScale.y, scaleMultiplier * player.position.y);
+        else transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
     }
-
 }
